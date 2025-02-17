10da49c4e8ebc4a839a933166656e96bbaca02a6
Scaxlibur <scaxlibur@gmail.com>
2025-02-17 23:44:09 +0800

    串口屏驱动开个头

---
+++ main/UART_screen.cpp
@@ -0,0 +1,30 @@
+#include "UART_screen.hpp"
+
+void UART_screen_class::init_uart(void){
+    if(uart_is_driver_installed(UART_NUM_SCREEN) == false){
+        ESP_ERROR_CHECK(uart_param_config(UART_NUM_SCREEN, &uart_config));
+        ESP_ERROR_CHECK(uart_set_pin(UART_NUM_SCREEN, UART_NUM_SCREEN_TX, UART_NUM_SCREEN_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+        ESP_ERROR_CHECK(uart_driver_install(UART_NUM_SCREEN, RX_BUF_SIZE * 2, 0, 0, NULL, 0));
+    };
+}
+
+int UART_screen_class::command_loop(void)
+{
+  int length = 0;
+  uart_get_buffered_data_len(UART_NUM_SCREEN, (size_t*)&length);
+  // 如果串口是空的直接返回
+  if (length == 0) return;
+  char received_chars[10];
+  vTaskDelay(1 / portTICK_PERIOD_MS);
+  // 从串口读取返回的数据，读取20个字符
+  uart_read_bytes(UART_NUM_SCREEN, received_chars, length, 100);
+
+  // 根据指令做不同动作9
+  if (received_chars[0] == 'F') // F指令设置
+  {
+
+  }
+  //  最后清空串口
+  while (length >= 0)
+    uart_flush(UART_NUM_SCREEN);
+}
---
+++ main/UART_screen.hpp
@@ -0,0 +1,44 @@
+#ifndef __UART_SCREEN_HPP__
+#define __UART_SCREEN_HPP__
+
+#include "driver/uart.h" 
+#include <string.h>
+#include "soc\gpio_num.h"
+#include "FreeRTOS.h"
+
+#define UART_NUM_SCREEN UART_NUM_1
+#define UART_NUM_SCREEN_TX UART_PIN_NO_CHANGE
+#define UART_NUM_SCREEN_RX UART_PIN_NO_CHANGE
+
+class UART_screen_class
+{
+private:
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT
+    };
+    static const int RX_BUF_SIZE = 1024;  //串口接收缓冲区大小
+    void init_uart(void);
+    int command_loop(void);
+
+public:
+    UART_screen_class();
+    ~UART_screen_class();
+    
+};
+
+UART_screen_class::UART_screen_class()
+{
+    init_uart();
+}
+
+UART_screen_class::~UART_screen_class()
+{
+}
+
+
+#endif
