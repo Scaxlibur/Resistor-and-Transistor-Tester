8868714ec5d661bfdbc3027aad1d913ce8267cbc
Scaxlibur <scaxlibur@gmail.com>
2025-02-25 21:55:50 +0800

    构造串口屏和测量仪之间的信息通道

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,28 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
+#include "UART_screen.hpp"
+
+
+typedef struct{
+    bool isRunning = false;
+    int msg = 0;
+}data4Tasks;
+
+QueueHandle_t screen_resistor_com_handle;  //串口屏和电阻测量模块的通信队列
+QueueHandle_t screen_transistor_com_handle;  //串口屏和三极管测量模块的通信队列
+
+TaskHandle_t screen_taskhHandle;
+TaskHandle_t resistor_taskhHandle;
+TaskHandle_t transistor_taskhHandle;
+
+void UARTscreen_task()
+{
+    UART_screen_class UARTscreen;
+}
 
 extern "C" void app_main(void)
 {
-  while (true)
-  {
-    vTaskDelay(45 / portTICK_PERIOD_MS);
-  }
+    while (true)
+    {
+        vTaskDelay(45 / portTICK_PERIOD_MS);
+    }
 }
